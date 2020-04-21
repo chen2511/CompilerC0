11fa227b1606488614002837b6821853caac5bff
chen2511 <1514300203@qq.com>
2020-04-21 15:05:06 +0800

    上个版本有大问题

--- CompilerC0-vs2019/global.h
+++ CompilerC0-vs2019/global.h
@@ -1,38 +1,48 @@
-#include "global.h"
-#include "scan.h"
-#include "parser.h"
+#pragma once
+#ifndef GLOBAL_H
+#define GLOBAL_H
 
-using namespace std;
-#pragma warning(disable:4996)
+#include <string>
+#include <iostream>
+#include <string.h>
 
-FILE* sourceFile;
+#define RESERVEDWORD_NUM 14
 
-int g_lineNumber = 0;
-_tToken g_token;
+//枚举类型TokenType：
+typedef enum {
+    //关键字：
+    CHAR, CONST, ELSE, FALSE, FOR,              //0-4
+    IF, INT, MAIN, PRINTF, RETURN,              //5-9
+    SCANF, TRUE, VOID, WHILE,                   //10-13
+    NUM, IDEN, LETTER, STRING,                  //14-17:数字、标识符、字符、字符串
+    PLUS, MINU, MULT, DIV,                      //18-21:+ - * /
+    AND, OR, NOT,                               //22-24:&& || !
+    LSS, LEQ, GRE, GEQ, NEQ, EQL,               //25-30关系运算符: < <= > >= != ==
+    ASSIGN, COMMA, SEMICOLON,                   //31-33: =  ,  ;
+    LBRACE, RBRACE, LBRACKET, RBRACKET,         //34-37:{ } [ ]
+    LPARENTHES, RPARENTHES,                     //38-39:( )                                         
+}TokenType;
+
+//Token:枚举类型TokenType、值string value
+typedef struct {
+    TokenType   opType;
+    char value[4096];
+}_tToken;
+
+//源文件
+extern FILE* sourceFile;
+//行号
+extern int g_lineNumber;
+//在buf中的指针；
+extern int g_lexBegin;
+//forward 指向下一个将要读取的字符；lexBegin表示该token开始的位置
+extern int g_forward;
+
+//token
+extern _tToken g_token;
+//是否到达文件末尾
+extern bool EOF_flag;
 
 
-int main(int argc, char* argv[])
-{
-    sourceFile = fopen("Text.txt", "r");
-    if (NULL == sourceFile) {
-        cout << "source file open failed!\n";
-    }
-    else {
-        cout << "source file open successfully!\n";
-    }
-
-    // 词法分析测试
-    //do {
-    //    getNextToken();
-    //    if (!EOF_flag)
-    //        cout << " token type: " << token.opType << " value: " << token.value << endl;
-    //} while (!EOF_flag);
-
-    parser();
-
-
-
-    return 0;
-}
-
+#endif
 
